fcbec2c5869ba1713c14987b5063f175e9a2f02f
Fabio Ayres <fabioja@insper.edu.br>
2022-08-18 12:22:34 -0300

    Reorganizei

---
+++ cascid/config.py
@@ -0,0 +1,6 @@
+# pylint: disable=missing-docstring
+from pathlib import Path
+
+DATA_DIR = Path(__file__).parents[2] / 'data'
+
+DATA_FILE = DATA_DIR / 'metadados.csv'
---
+++ setup.py
@@ -0,0 +1,9 @@
+# pylint: disable=missing-docstring
+from setuptools import setup, find_packages
+
+setup(
+    name='CASCID',
+    version='0.1',
+    packages=find_packages(),
+    description='Computer-Aided Skin Cancer Identification and Diagnostics',
+)
