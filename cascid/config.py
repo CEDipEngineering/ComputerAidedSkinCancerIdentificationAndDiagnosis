829caa93082f029f664e3c0f0e386279d52e69ee
CEDipEngineering <cadudip@hotmail.com>
2022-08-19 18:45:53 -0300

    Moved useful functions to module and removed notebook

--- cascid/config.py
+++ cascid/config.py
@@ -1,6 +1,10 @@
 # pylint: disable=missing-docstring
 from pathlib import Path
 
-DATA_DIR = Path(__file__).parents[2] / 'data'
+DATA_DIR = Path(__file__).parents[0]
 
 DATA_FILE = DATA_DIR / 'metadados.csv'
+
+IMAGE_DIR = DATA_DIR / 'images'
+
+
