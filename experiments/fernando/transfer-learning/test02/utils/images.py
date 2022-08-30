43ba8a56522843f3474d67c678753d8413469218
Fernando Fincatti <fernandofincatti@pop-os.localdomain>
2022-08-30 18:58:48 -0300

    fix: improving load images module

--- experiments/fernando/transfer-learning/test02/utils/images.py
+++ experiments/fernando/transfer-learning/test02/utils/images.py
@@ -6,8 +6,9 @@ def get_file_names_in_path(path):
 
 def read_image_file(file_path):
     image = cv.imread(file_path)
-    image_resized = cv.resize(image, (64, 64))
-    image_normalized = image_resized / 255
+    image_resized = cv.resize(image, (128, 128))
+    image_rgb = cv.cvtColor(image_resized, cv.COLOR_BGR2RGB)
+    image_normalized = image_rgb / 255
     return image_normalized
 
 def read_all_images_in_path(path):
@@ -20,9 +21,9 @@ def read_all_images_in_path(path):
     return read_files, files_to_read
 
 def load_images(path):
-    images_01, file_names = read_all_images_in_path(path+"imgs_part_1")
-    images_02, file_names = read_all_images_in_path(path+"imgs_part_2")
-    images_03, file_names = read_all_images_in_path(path+"imgs_part_3")
+    images_01, file_names_01 = read_all_images_in_path(path+"imgs_part_1")
+    images_02, file_names_02 = read_all_images_in_path(path+"imgs_part_2")
+    images_03, file_names_03 = read_all_images_in_path(path+"imgs_part_3")
     all_images = images_01 + images_02 + images_03
-    all_file_names = file_names + file_names + file_names
+    all_file_names = file_names_01 + file_names_02 + file_names_03
     return all_images, all_file_names
