7bb75406164a1d7f1172495d2792aac74bc619c7
CEDipEngineering <cadudip@hotmail.com>
2022-09-05 14:52:54 -0300

    Continue segmentation and start hair removal

--- cascid/image_sampling.py
+++ cascid/image_sampling.py
@@ -35,7 +35,7 @@ def plot_age_distribution(df: pd.DataFrame) -> plt.figure:
     # fig.show()
     return fig
 
-def show_sample_pictures(df: pd.DataFrame, n_samples=9) -> plt.figure:
+def show_sample_pictures(df: pd.DataFrame, n_samples: int = 9 , random_seed: int = None) -> plt.figure:
     '''
     Function to show <n_samples> images from the dataframe.
     Dataframe must have the columns 'img_id', 'diagnostic', 'patient_id'
@@ -50,7 +50,7 @@ def show_sample_pictures(df: pd.DataFrame, n_samples=9) -> plt.figure:
     # Show 3 pictures of people with Melanoma
     show_sample_pictures(df[ (df['diagnostic'] == "MEL")], n_samples=3)
     '''
-    microdf = df.sample(n_samples).reset_index()
+    microdf = df.sample(n_samples, random_state=random_seed).reset_index()
     imgs = microdf['img_id'].to_list()
     diganosis = microdf['diagnostic'].to_list()
     patients = microdf['patient_id'].to_list()
@@ -58,15 +58,15 @@ def show_sample_pictures(df: pd.DataFrame, n_samples=9) -> plt.figure:
     axList = []
     for i in range(n_samples):
         axList.append(fig.add_subplot((2+n_samples)//3,3,i+1))
-        filename = IMAGE_DIR.stem + "/" + imgs[i]
-        print(filename)
+        filename = str(IMAGE_DIR) + "/" + imgs[i]
+        # print(filename)
         img = cv2.imread(filename)[:,:,::-1]
         axList[i].imshow(img)
         axList[i].set_title("Patient:{0} | Diagnosis:{1}".format(patients[i], diganosis[i]))
     # fig.show()
     return fig
 
-def show_histograms_by_picture(df: pd.DataFrame, n_samples=3) -> plt.figure:
+def show_histograms_by_picture(df: pd.DataFrame, n_samples: int = 3, random_seed: int = None) -> plt.figure:
     '''
     Function to show <n_samples> images from dataframe beside their color histogram.
     Dataframe must have the columns 'img_id', 'diagnostic', 'patient_id'
@@ -81,7 +81,7 @@ def show_histograms_by_picture(df: pd.DataFrame, n_samples=3) -> plt.figure:
     # Show 3 pictures of people with Melanoma, and the color histogram of said pictures
     show_histograms_by_picture(df[ (df['diagnostic'] == "MEL")], n_samples=3)
     '''
-    microdf = df.sample(n_samples).reset_index()
+    microdf = df.sample(n_samples, random_state=random_seed).reset_index()
     imgs = microdf['img_id'].to_list()
     diganosis = microdf['diagnostic'].to_list()
     patients = microdf['patient_id'].to_list()
@@ -90,7 +90,7 @@ def show_histograms_by_picture(df: pd.DataFrame, n_samples=3) -> plt.figure:
     for i in range(n_samples):
         axList[2*i] = fig.add_subplot(n_samples,2,2*i+1)
         axList[2*i+1] = fig.add_subplot(n_samples,2,2*i+2)
-        filename = IMAGE_DIR.stem + "/" + imgs[i]
+        filename = str(IMAGE_DIR) + "/" + imgs[i]
         img = cv2.imread(filename)[:,:,::-1]
         axList[2*i].imshow(img)
         axList[2*i].set_title("Patient:{0} | Diagnosis:{1}".format(patients[i], diganosis[i]))
@@ -103,7 +103,7 @@ def show_histograms_by_picture(df: pd.DataFrame, n_samples=3) -> plt.figure:
         axList[2*i+1].legend()
     return fig
 
-def apply_transform(df: pd.DataFrame, n_samples=3, f: Callable[[np.ndarray,], np.ndarray] = lambda x: x, random_seed = None) -> plt.figure:
+def apply_transform(df: pd.DataFrame, n_samples=3, f: Callable[[np.ndarray,], np.ndarray] = lambda x: x, random_seed: int = None) -> plt.figure:
     '''
     Function to transform <n_samples> images from dataframe. Display original image beside transformed result.
     Function f must receive and return a single image, which is a numpy ndarray, as read by opencv.
