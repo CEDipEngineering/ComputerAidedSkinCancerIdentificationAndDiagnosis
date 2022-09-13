845dfba638998b0dc247687974744b1f61b631fa
Fabio Ayres <fabioja@insper.edu.br>
2022-09-13 10:47:18 -0300

    Exemplo de pre-calculo de features

--- cascid/config.py
+++ cascid/config.py
@@ -1,10 +1,8 @@
 # pylint: disable=missing-docstring
 from pathlib import Path
 
-DATA_DIR = Path(__file__).parents[0]
+DATA_DIR = Path(__file__).parents[2] / 'data' / 'PAD-UFES'
 
-DATA_FILE = DATA_DIR / 'metadados.csv'
+DATA_FILE = DATA_DIR / 'metadata.csv'
 
 IMAGE_DIR = DATA_DIR / 'images'
-
-
--- experiments/fernando/transfer-learning/test02/utils/images.py
+++ experiments/fernando/transfer-learning/test02/utils/images.py
@@ -1,8 +1,10 @@
 from os import listdir
 import cv2 as cv
 
+
 def get_file_names_in_path(path):
     return [file for file in listdir(path)]
+
 
 def read_image_file(file_path, shape):
     image = cv.imread(file_path)
@@ -11,19 +13,24 @@ def read_image_file(file_path, shape):
     image_normalized = image_rgb / 255
     return image_normalized
 
+
 def read_all_images_in_path(path, shape):
     files_to_read = get_file_names_in_path(path)
     read_files = list()
     for file in files_to_read:
-        dir_to_file = "{0}/{1}".format(path, file) 
+        dir_to_file = "{0}/{1}".format(path, file)
         image_array = read_image_file(dir_to_file, shape)
         read_files.append(image_array)
     return read_files, files_to_read
 
+
 def load_images(path, shape):
-    images_01, file_names_01 = read_all_images_in_path(path+"imgs_part_1", shape)
-    images_02, file_names_02 = read_all_images_in_path(path+"imgs_part_2", shape)
-    images_03, file_names_03 = read_all_images_in_path(path+"imgs_part_3", shape)
+    images_01, file_names_01 = read_all_images_in_path(path + "imgs_part_1",
+                                                       shape)
+    images_02, file_names_02 = read_all_images_in_path(path + "imgs_part_2",
+                                                       shape)
+    images_03, file_names_03 = read_all_images_in_path(path + "imgs_part_3",
+                                                       shape)
     all_images = images_01 + images_02 + images_03
     all_file_names = file_names_01 + file_names_02 + file_names_03
     return all_images, all_file_names
