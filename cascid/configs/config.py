343370e43dc4d564529e89305924921967e09df3
CEDipEngineering <cadudip@hotmail.com>
2022-09-14 09:06:45 -0300

    Update download dir

--- cascid/configs/config.py
+++ cascid/configs/config.py
@@ -3,5 +3,5 @@ from pathlib import Path
 from os import path
 import sys
 
-DATA_DIR = Path(path.split(sys.executable)[0]) / "cascid_data" # Dir to install dataset, based on python install location
-    
+DATA_DIR = Path.home() / ".cascid_data" # Dir to install dataset, based on python install location
+DATA_DIR.mkdir(exist_ok=True, parents=True)
