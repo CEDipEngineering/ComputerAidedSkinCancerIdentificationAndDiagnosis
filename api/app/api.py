46b68c64daeb82f0f5722a60555ef2527172f760
CEDipEngineering <cadudip@hotmail.com>
2022-09-19 11:54:05 -0300

    API base implementation completed

--- api/app/api.py
+++ api/app/api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Form
-from model import PredictiveModel
+from app.model import PredictiveModel
 import uuid
 import base64
 from cascid.configs import config
@@ -15,15 +15,16 @@ API_DATA.mkdir(parents=True, exist_ok=True)
 model = PredictiveModel()
 
 @app.post("/upload")
-def upload(filedata: str = Form(...)):
-    image_as_bytes = str.encode(filedata)  # convert string to bytes
+def upload(imagedata: str = Form(...)):
+    image_as_bytes = str.encode(imagedata)  # convert string to bytes
     img_recovered = base64.b64decode(image_as_bytes)  # decode base64string
     try:
-        fn = uuid.uuid4() + ".jpg"
+        fn = str(uuid.uuid4()) + ".jpg"
         with open(API_DATA / fn, "wb") as f:
             f.write(img_recovered)
         db.append(fn)
-    except Exception:
+    except Exception as e:
+        print(e)
         return {"message": "There was an error uploading the file"}
     
     return {"path": fn} 
@@ -35,6 +36,6 @@ async def read_file(fn):
         image = cv2.cvtColor(cv2.imread(str(API_DATA/fn)), cv2.COLOR_BGR2RGB)
         pred = model.predict_proba(image)
         print(pred)
-    except FileNotFoundError:
+    except Exception:
         return {"Error" : "File not found, check your request path"}
-    return str(pred)
+    return {"prediction": str(pred)}
