c0633a4aa7f3f8600bf2f5769d468a61ac0d612f
CEDipEngineering <cadudip@hotmail.com>
2022-10-10 09:17:18 -0300

    Add progress bar, tqdm dependency

--- experiments/dip/cv/starmap.py
+++ experiments/dip/cv/starmap.py
@@ -1,20 +1,24 @@
 from multiprocessing.pool import ThreadPool
+from turtle import update
 from typing import List, Callable
 import numpy as np
+from tqdm import tqdm
 
 def apply_params_async(transform: Callable, args: np.ndarray):
     with ThreadPool(16) as TP:
-        result = TP.starmap(transform, args)
-    return result
+        results = list(tqdm(TP.imap(transform, args), total=len(args)))
+        TP.close()
+        TP.join()
+    return results
 
 def sample_func(*args):
     return sum(args)
 
 def main():
-    sample_data = np.random.randint(0,5, (5,5))
+    sample_data = np.random.randint(0,5, (100000,2000))
     result = apply_params_async(sample_func, sample_data)
-    print(sample_data)
-    print(result)
+    # print(sample_data)
+    # print(result)
     return
 
 if __name__ == "__main__":
