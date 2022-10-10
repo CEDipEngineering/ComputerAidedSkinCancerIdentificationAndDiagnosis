3d2101bdb36ba71e53e0b54a7e2a3aa75fded31c
CEDipEngineering <cadudip@hotmail.com>
2022-10-10 12:51:06 -0300

    Add comments

--- experiments/dip/cv/starmap.py
+++ experiments/dip/cv/starmap.py
@@ -6,16 +6,18 @@ from tqdm import tqdm
 
 def apply_params_async(transform: Callable, args: np.ndarray):
     with ThreadPool(16) as TP:
-        results = list(tqdm(TP.imap(transform, args), total=len(args)))
-        TP.close()
-        TP.join()
+        results = list(tqdm(TP.imap(transform, args), total=len(args))) # Use tqdm iterator to update progress bar automatically
+        TP.close() # Inform pool there will be no further job submissions
+        TP.join() # Await pool jobs
     return results
 
 def sample_func(*args):
+    # Sample function, can receive any amount of positional arguments, will return their sum
     return sum(args)
 
 def main():
-    sample_data = np.random.randint(0,5, (100000,2000))
+    # Example of starmap use
+    sample_data = np.random.normal(0,1, (100000,2000))
     result = apply_params_async(sample_func, sample_data)
     # print(sample_data)
     # print(result)
