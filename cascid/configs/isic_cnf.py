910070eef43d42ad696a57df196ff91cc1d5c687
CEDipEngineering <cadudip@hotmail.com>
2022-11-03 10:22:28 -0300

    Preprocessing tweaks

--- cascid/configs/isic_cnf.py
+++ cascid/configs/isic_cnf.py
@@ -5,5 +5,7 @@ SEARCH_URL = BASE_API_URL + "/images/search"
 IMAGES_DIR = config.DATA_DIR / "ISIC"
 IMAGES_DIR.mkdir(parents=True, exist_ok=True)
 METADATA = config.DATA_DIR / "ISIC" / "metadata.csv"
+HAIRLESS_QUANTIZED_DIR = IMAGES_DIR / 'hairless_quantized'
+HAIRLESS_QUANTIZED_DIR.mkdir(parents=True, exist_ok=True)
 HAIRLESS_DIR = IMAGES_DIR / 'hairless'
 HAIRLESS_DIR.mkdir(parents=True, exist_ok=True)
--- cascid/datasets/pad_ufes/images.py
+++ cascid/datasets/pad_ufes/images.py
@@ -14,7 +14,12 @@ from cascid.datasets.pad_ufes import database, images
 from cascid.image.image_preprocessing import adaptive_hair_removal
 import os
 
+from cascid.image.image_preprocessing import adaptive_hair_removal2, color_quantization
+
 _warning_load_image_without_shape = False
+
+def remove_and_quantize(img):
+    return color_quantization(adaptive_hair_removal2(img))
 
 def _load_image(img_name: str, prefix: Path, shape: Tuple[int, int] = None) -> np.ndarray:
     global _warning_load_image_without_shape
@@ -75,6 +80,61 @@ def get_hairless_image(img_name: str, image_shape: Tuple[int, int] = None):
     print("Preprocessing a list of images can be done using cascid.image.apply_preprocessing.remove_hair(img_list)")
     print("\n"*3)
     raise FileNotFoundError("Supplied image does not have a preprocessed file, read above error")
+
+def get_hq_image(img_name: str, image_shape: Tuple[int, int] = None):
+    """
+    ## Function used to read preprocessed image from disk. Used to abstract directory structure from user.
+    
+    Args:
+    - img_name: string, as found in 'img_id' column in metadata, such as 'PAT_2046_4323_394.png'.
+    - image_shape: Tuple of two integers, size of image array output. Defaults to (128, 128).
+
+    Raises: FileNotFoundError if img_name has not been preprocessed yet
+
+    Example:
+
+    ## Load first image from dataset:    
+    df = datasets.pad_ufes.database.get_df() # Get metadata
+    
+    img_raw = get_hairless_image(df.iloc[0]['img_id'], (128, 128)) # Get first image hq 
+
+    """
+    if (pad_ufes_cnf.HAIRLESS_QUANTIZED_DIR / img_name).exists():
+        return _load_image(img_name, pad_ufes_cnf.HAIRLESS_QUANTIZED_DIR, image_shape)
+    print("Supplied image name '{}' has no preprocessed file found".format(img_name))
+    print("Please, apply preprocessing to all images that will be used beforehand")
+    print("Preprocessing a list of images can be done using cascid.image.apply_preprocessing.remove_hair(img_list)")
+    print("\n"*3)
+    raise FileNotFoundError("Supplied image does not have a preprocessed file, read above error")
+
+def remove_hair_and_quantize(img_list: List[str]) -> None:
+    """
+    Preprocessing function, used to remove hair from list of images, and save preprocessed results in pad_ufes_cnf.HAIRLESS_QUANTIZED_DIR.
+    Warning, this processing is done with mutiple threads, and as such should be done with larger numbers of images. 
+    Calling this function repeatedly with a single image on the list will result in extremely slow performance.
+
+    Args:
+    img_list: List of strings of image names, as found in metadata, such as ['PAT_2046_4323_394.png'].
+    """
+
+    prepend_output_dir = lambda x: str(pad_ufes_cnf.HAIRLESS_QUANTIZED_DIR / x)
+    # Arg 1
+    orig_names = np.array(img_list).reshape(-1,1)
+    # Arg 2
+    target_names = np.array([prepend_output_dir(i) for i in img_list]).reshape(-1,1)
+    # Arg 3
+    func = np.array(list(repeat(remove_and_quantize,len(orig_names))), dtype=object).reshape(-1,1)
+    # Stack into list
+    args = np.hstack([orig_names, target_names, func])
+    # Run
+    print("Beginning transformations, this may take a while...")
+    start = time.perf_counter()
+    result = _apply_params_async(_process_and_save, args, nthreads=8)
+    elapsed = time.perf_counter()-start
+    hour=int(elapsed//3600)
+    minute=int((elapsed%3600)//60)
+    seconds=float((elapsed%3600)%60)
+    print("Finished transformations after {:d}h{:02d}min{:.02f}s".format(hour,minute,seconds))
 
 def remove_hair(img_list: List[str]) -> None:
     """
